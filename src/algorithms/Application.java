85b90f415e7aea7f03cce27923ac54e3656f0eb5
bobbygabriel <bgabriel@bellarmine.edu>
2022-12-11 23:19:25 -0500

    12/11/22 - Bobby - added more information to the output and finished commenting - 2 hours

--- src/algorithms/Application.java
+++ src/algorithms/Application.java
@@ -11,6 +11,8 @@
 
 package algorithms;
 
+import java.util.Arrays;
+
 public class Application {
 
 	/**
@@ -18,23 +20,59 @@ public class Application {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		// Creating the Unique array to be used for the selection sort and shell sort
+				
         CreateUniqueArray c = new CreateUniqueArray();
-        int randomArray[] = c.createArray(950, 800);
+
+        System.out.println("-----Differences in Creating a Unique Random Array-----");
+
+        // 2 examples to show wall-clock time difference on having a large gap vs small gap with randomRange and array length
+        long startTime = System.nanoTime();
+        c.createArray(10000, 10000);
+        long endTime = System.nanoTime();
+        long duration = (endTime - startTime)/1000000;
+        //System.out.println(Arrays.toString(example));
+        System.out.println("Array Size 10,000 with Range of 10,000");
+        System.out.println("Execution time: " + duration + "ms\n");
+        
+        
+        startTime = System.nanoTime();
+        c.createArray(100000, 10000);
+        endTime = System.nanoTime();
+        duration = (endTime - startTime)/1000000;
+        //System.out.println(Arrays.toString(example));
+        System.out.println("Array Size 10,000 with Range of 100,000");
+        System.out.println("Execution time: " + duration + "ms");
+        System.out.println("\n\n");
+        
+        
+        
+        // Creating the unique array to be used for both Selection sort and Shell sort
+        // Also measuring the time it takes to complete so it can be added to the total execution time of the sorting algorithms
+        long totalArrayStartTime = System.nanoTime();
+        
+        int randomRange = 200000;
+        int n = 100000;
+        int randomArray[] = c.createArray(randomRange, n);
+        
+        long totalArrayEndTime = System.nanoTime();
+        long totalArrayDuration = (totalArrayEndTime - totalArrayStartTime)/1000000;
+        System.out.println("-----Sorting Algorithms with array size: " + n + " and range: " + randomRange + " -----\n");
 
 
-
-
+        
+        
+        
+        
+        
         // ------------  SELECTION SORT ------------
-        System.out.println("Selection Sort");
+        System.out.println("-----Selection Sort-----");
         SelectionSort s = new SelectionSort();
 
         // Tracking the wall-clock time of the given selection sort
-        long startTime = System.nanoTime();
+        startTime = System.nanoTime();
         int selectionSortedArray[] = s.sort(randomArray);
-        long endTime = System.nanoTime();
-        long duration = (endTime - startTime)/1000000;
+        endTime = System.nanoTime();
+        duration = (endTime - startTime)/1000000;
 
         // Prints the sorted array
         //s.printArray(selectionSortedArray);
@@ -42,15 +80,20 @@ public class Application {
         // Returns the number of iterations made in selection sort
         s.comparisons(selectionSortedArray);
 
+        // Calculates time to create the array + run the sorting algorithm
+        long totalDuration = duration + totalArrayDuration;
+        
         // Displays the wall-clock time
         System.out.println("Execution time: " + duration + "ms");
+        System.out.println("Execution time including the creation of the array: " + totalDuration + "ms");
+        System.out.println("\n");
 
+        
+        
 
-
-
-
+        
         // ------------  SHELL SORT -------------
-        System.out.println("\nShell Sort");
+        System.out.println("-----Shell Sort-----");
         ShellSort shell = new ShellSort();
 
         // Tracking the wall-clock time of the given selection sort
@@ -65,8 +108,14 @@ public class Application {
         // Returns the number of iterations made in shell sort
         shell.comparisons(shellSortedArray);
 
+        // Calculates time to create the array + run the sorting algorithm
+        totalDuration = duration + totalArrayDuration;
+        
         // Returns the wall-clock time
         System.out.println("Execution time: " + duration + "ms");
+        System.out.println("Execution time including the creation of the array: " + totalDuration + "ms");
+        
+        
         
 	}//end main
 
--- src/algorithms/ShellSort.java
+++ src/algorithms/ShellSort.java
@@ -26,7 +26,8 @@ public class ShellSort {
         {
              /*
               * Do a gapped insertion sort for this gap size.
-              * The first gap elements a[0..gap-1] are already in gapped order, keep adding one more element until the entire array is gap sorted
+              * The first gap elements a[0..gap-1] are already in gapped order, 
+              * keep adding one more element until the entire array is gap sorted
               */
             for (int i = gap; i < n; i += 1)
             {
